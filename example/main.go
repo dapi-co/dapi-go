39f8d454e17bd6cfb8593c05d04ceddf78c5ffad
Ahmad Sameh <8120013+asmsh@users.noreply.github.com>
2021-02-07 10:43:32 +0200

    GDT-862: Updated the example

--- example/main.go
+++ example/main.go
@@ -4,17 +4,23 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/dapi-co/dapi-go/handler"
+	"github.com/dapi-co/dapi-go/app"
+	"github.com/dapi-co/dapi-go/config"
 )
 
 func main() {
 
-	err := http.ListenAndServe(":8085", http.HandlerFunc(handler.HandleDapiRequests))
+	myConfig := config.Config{
+		AppSecret: "MY_APP_SECRET",
+	}
+	myApp := app.NewDapiApp(myConfig)
+	err := http.ListenAndServe(":8085", http.HandlerFunc(myApp.HandleDapiRequests))
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-// run the main function above, then call: http://localhost:8085/
+// set the appSecret for your app in the AppSecret field of the myConfig var
+// above, then run the main function above, then call: http://localhost:8085/
 // and set the 'action' field in the body to the endpoint you want to call,
 // and add the values that's wanted by that endpoint.

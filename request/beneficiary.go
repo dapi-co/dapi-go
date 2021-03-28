4c83f6d00fc8f58ff38d7e7b89c5b22d27aacd64
Kamil Kamili <kamili.kamil@outlook.com>
2021-03-28 16:30:27 +0400

    CS-69: Unified the Benefs needed in CB and TA

--- request/beneficiary.go
+++ request/beneficiary.go
@@ -2,18 +2,24 @@ package request
 
 import "github.com/dapi-co/dapi-go/response"
 
-// BeneficiaryInfo represents the beneficiary to be created
+// CreateBeneficiaryInfo represents the beneficiary to be created
+type CreateBeneficiaryInfo struct {
+	BeneficiaryInfo
+	Type          response.BeneficiaryType    `json:"type,omitempty"`
+}
+
+
+// BeneficiaryInfo represents the beneficiary
 type BeneficiaryInfo struct {
 	Name          string                      `json:"name,omitempty"`
-	Type          response.BeneficiaryType    `json:"type,omitempty"`
 	Address       response.BeneficiaryAddress `json:"address,omitempty"`
 	Country       string                      `json:"country,omitempty"`
-	SortCode      string                      `json:"sortCode,omitempty"`
 	BranchAddress string                      `json:"branchAddress,omitempty"`
 	BankName      string                      `json:"bankName,omitempty"`
 	BranchName    string                      `json:"branchName,omitempty"`
 	PhoneNumber   string                      `json:"phoneNumber,omitempty"`
 	Iban          string                      `json:"iban,omitempty"`
+	RoutingNumber          string                      `json:"routingNumber,omitempty"`
 	SwiftCode     string                      `json:"swiftCode,omitempty"`
 	AccountNumber string                      `json:"accountNumber,omitempty"`
 }

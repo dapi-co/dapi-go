0da6e0594a55a4be0f831722600f030225e0588d
Kamil Kamili <kamili.kamil@outlook.com>
2021-03-28 16:29:46 +0400

    CS-69: Removed changes for dd

--- app/app.go
+++ app/app.go
@@ -2,102 +2,19 @@ package app
 
 import (
 	"encoding/json"
+	"github.com/dapi-co/dapi-go/config"
+	"github.com/dapi-co/dapi-go/constants"
+	"github.com/dapi-co/dapi-go/request"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"time"
-
-	"github.com/dapi-co/dapi-go/auth"
-	"github.com/dapi-co/dapi-go/config"
-	"github.com/dapi-co/dapi-go/data"
-	"github.com/dapi-co/dapi-go/payment"
-	"github.com/dapi-co/dapi-go/response"
 )
 
 type DapiApp struct {
-	config config.Config
+	Config *config.Config
 }
 
-func NewDapiApp(config config.Config) *DapiApp {
-	return &DapiApp{config: config}
-}
-
-func (app *DapiApp) ExchangeToken(
-	accessCode string,
-	connectionID string,
-) (*response.ExchangeTokenResponse, error) {
-	a := auth.Auth{Config: &app.config}
-	return a.ExchangeToken(accessCode, connectionID)
-}
-
-// TODO: add DelinkUser
-
-func (app *DapiApp) GetIdentity(
-	accessToken string,
-	userSecret string,
-	userInputs []response.UserInput,
-	operationID string,
-) (*response.IdentityResponse, error) {
-	d := data.Data{Config: &app.config}
-	return d.GetIdentity(accessToken, userSecret, userInputs, operationID)
-}
-
-func (app *DapiApp) GetAccounts(
-	accessToken string,
-	userSecret string,
-	userInputs []response.UserInput,
-	operationID string,
-) (*response.AccountsResponse, error) {
-	d := data.Data{Config: &app.config}
-	return d.GetAccounts(accessToken, userSecret, userInputs, operationID)
-}
-
-func (app *DapiApp) GetBalance(
-	accessToken string,
-	userSecret string,
-	accountID string,
-	userInputs []response.UserInput,
-	operationID string,
-) (*response.BalanceResponse, error) {
-	d := data.Data{Config: &app.config}
-	return d.GetBalance(accessToken, userSecret, accountID, userInputs, operationID)
-}
-
-func (app *DapiApp) GetTransactions(
-	accessToken string,
-	userSecret string,
-	accountID string,
-	fromDate time.Time,
-	toDate time.Time,
-	userInputs []response.UserInput,
-	operationID string,
-) (*response.TransactionsResponse, error) {
-	d := data.Data{Config: &app.config}
-	return d.GetTransactions(accessToken, userSecret, accountID, fromDate, toDate, userInputs, operationID)
-}
-
-func (app *DapiApp) GetBeneficiaries(
-	accessToken string,
-	userSecret string,
-	userInputs []response.UserInput,
-	operationID string,
-) (*response.BeneficiariesResponse, error) {
-	p := payment.Payment{Config: &app.config}
-	return p.GetBeneficiaries(accessToken, userSecret, userInputs, operationID)
-}
-
-func (app *DapiApp) CreateTransfer(
-	accessToken string,
-	userSecret string,
-	transfer payment.Transfer,
-	userInputs []response.UserInput,
-	operationID string,
-) (*response.TransferResponse, error) {
-	p := payment.Payment{Config: &app.config}
-	return p.CreateTransfer(accessToken, userSecret, transfer, userInputs, operationID)
-}
-
-func (app *DapiApp) HandleDapiRequests(rw http.ResponseWriter, req *http.Request) {
+func (app *DapiApp) HandleSDKDapiRequests(rw http.ResponseWriter, req *http.Request) {
 	defer req.Body.Close()
 
 	// read the body sent
@@ -116,7 +33,7 @@ func (app *DapiApp) HandleDapiRequests(rw http.ResponseWriter, req *http.Request
 		rw.Write([]byte(`{"success":false,"msg":"Bad request","type":"BAD_REQUEST","status":"failed"}`))
 		return
 	}
-	bodyMap["appSecret"] = app.config.AppSecret
+	bodyMap["appSecret"] = app.Config.AppSecret
 
 	// marshal the new body back to json
 	body, err = json.Marshal(bodyMap)
@@ -127,7 +44,7 @@ func (app *DapiApp) HandleDapiRequests(rw http.ResponseWriter, req *http.Request
 	}
 
 	// forward the request to be handled
-	resp, err := handleDapiRequest(body, req.Header)
+	resp, err := request.DapiRequest(body, constants.DD_URL, req.Header)
 	if err != nil {
 		log.Printf("an error happened while handling the request. err: %v\n", err)
 		rw.WriteHeader(http.StatusInternalServerError)

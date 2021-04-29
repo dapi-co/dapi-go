4f088695dba79f1363de0e4fc8b2b9b745560f5c
Ahmad Sameh <8120013+asmsh@users.noreply.github.com>
2021-04-29 19:20:26 +0200

    CS-69: Introduced the UserInputID type

--- response/userInputs.go
+++ response/userInputs.go
@@ -1,8 +1,19 @@
 package response
 
+type UserInputID string
+
+const (
+	UserInputOTP            = "otp"
+	UserInputSecretQuestion = "secret_question"
+	UserInputCaptcha        = "captcha"
+	UserInputPin            = "pin"
+	UserInputConfirmation   = "confirmation"
+	UserInputToken          = "token"
+)
+
 type UserInput struct {
-	ID     string `json:"id,omitempty"`
-	Query  string `json:"query,omitempty"`
-	Answer string `json:"answer,omitempty"`
-	Index  int    `json:"index,omitempty"`
+	ID     UserInputID `json:"id,omitempty"`
+	Query  string      `json:"query,omitempty"`
+	Answer string      `json:"answer,omitempty"`
+	Index  int         `json:"index,omitempty"`
 }

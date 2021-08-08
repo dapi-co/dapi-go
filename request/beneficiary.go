0c8f15b24e4cc81e4fd21edadb51d2f7ebdfd20b
Ahmad Sameh <8120013+asmsh@users.noreply.github.com>
2021-08-08 18:05:12 +0200

    CS-471: Added the missing Nickname field to the beneficiary type

--- request/beneficiary.go
+++ request/beneficiary.go
@@ -5,13 +5,13 @@ import "github.com/dapi-co/dapi-go/response"
 // CreateBeneficiaryInfo represents the beneficiary to be created
 type CreateBeneficiaryInfo struct {
 	BeneficiaryInfo
-	Type          response.BeneficiaryType    `json:"type,omitempty"`
+	Type response.BeneficiaryType `json:"type,omitempty"`
 }
-
 
 // BeneficiaryInfo represents the beneficiary
 type BeneficiaryInfo struct {
 	Name          string                      `json:"name,omitempty"`
+	Nickname      string                      `json:"nickname,omitempty"`
 	Address       response.BeneficiaryAddress `json:"address,omitempty"`
 	Country       string                      `json:"country,omitempty"`
 	BranchAddress string                      `json:"branchAddress,omitempty"`
@@ -19,7 +19,7 @@ type BeneficiaryInfo struct {
 	BranchName    string                      `json:"branchName,omitempty"`
 	PhoneNumber   string                      `json:"phoneNumber,omitempty"`
 	Iban          string                      `json:"iban,omitempty"`
-	RoutingNumber          string                      `json:"routingNumber,omitempty"`
+	RoutingNumber string                      `json:"routingNumber,omitempty"`
 	SwiftCode     string                      `json:"swiftCode,omitempty"`
 	AccountNumber string                      `json:"accountNumber,omitempty"`
 }

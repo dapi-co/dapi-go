567f36f39da5e95a53221b2cba9252da86501f59
kc-sabal <sabal@dapi.co>
2024-03-26 19:06:28 +0545

    BE-2736: Added helper functions for webhooks

--- utils/utils.go
+++ utils/utils.go
@@ -1,8 +1,14 @@
 package utils
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
+	"encoding/base64"
 	"math/rand"
+	"net/http"
 	"time"
+
+	svix "github.com/svix/svix-webhooks/go"
 )
 
 func GenerateRandomString(length int) string {
@@ -17,4 +23,38 @@ func GenerateRandomString(length int) string {
 	}
 
 	return result
+}
+
+func DecryptWebhookData(aesKey string, encryptedData string) (string, error) {
+	key, err := base64.StdEncoding.DecodeString(aesKey)
+	if err != nil {
+		return "", err
+	}
+
+	encDataBuffer, err := base64.StdEncoding.DecodeString(encryptedData)
+	if err != nil {
+		return "", err
+	}
+
+	iv := encDataBuffer[:aes.BlockSize]
+	encryptedText := encDataBuffer[aes.BlockSize:]
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+
+	mode := cipher.NewCFBDecrypter(block, iv)
+	decrypted := make([]byte, len(encryptedText))
+	mode.XORKeyStream(decrypted, encryptedText)
+
+	return string(decrypted), nil
+}
+
+func VerifyWebhook(secret string, payload []byte, headers http.Header) bool {
+	wh, _ := svix.NewWebhook(secret)
+	if err := wh.Verify(payload, headers); err != nil {
+		return false
+	}
+	return true
 }
